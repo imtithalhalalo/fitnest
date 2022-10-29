2ef347141af49dd1471ee47b6f33e540a12ab205
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-29 12:59:51 +0300

    backend: adding user routes

--- fitnest-backend/routes/api.php
+++ fitnest-backend/routes/api.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(["middleware"=> "cors"], function(){
 
+    Route::group(["middleware" => "user.role"], function(){
+        Route::post('/add_info',[UserController::class, 'addInfo'])->name('add-info');
+        Route::put('/update_info',[UserController::class, 'updateInfo'])->name('update-info');
+        Route::put('/update_profile',[UserController::class, 'updateProfile'])->name('update-profile');
+        Route::post('/upload_image',[UserController::class, 'uploadImage'])->name('upload-image');
+        Route::post('/add_water_intake',[UserController::class, 'addWaterIntake'])->name('add-water-intake');
+    });
+
     //auth routes
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
