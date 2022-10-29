9c8ea3a55482dbfc420af22f24b792b20f1230d4
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-29 14:55:17 +0300

    backend: adding eloquent user info

--- fitnest-backend/app/Models/User.php
+++ fitnest-backend/app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-
+use App\Models\UserInfo;
 class User extends Authenticatable implements JWTSubject {
     use HasFactory, Notifiable;
 
@@ -30,5 +30,9 @@ class User extends Authenticatable implements JWTSubject {
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    public function info() {
+        return $this->hasOne(UserInfo::class);
     }
 }
