8564e625d3e034f34bae7c9ea463fd07898b3fc2
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-29 12:49:16 +0300

    backend: adding users info table

---
+++ fitnest-backend/database/migrations/2022_10_29_082305_create_users_info_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_info', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->references('id')->on('users');
+            $table->integer('weight_goal');
+            $table->integer('height');
+            $table->date('dob');
+            $table->integer('age');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_info');
+    }
+}
