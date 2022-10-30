7616be53e7194c857f4b2fc08243d031213a71b4
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 12:32:22 +0200

    backend: adding meal model

---
+++ fitnest-backend/app/Models/Meal.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Ingredient;
+
+class Meal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'ingredient',
+        'calories',
+        'fats',
+        'protein',
+        'image',
+        'category',
+        'user_id'
+    ];
+    
+    public function ingredients() {
+        return $this->belongsToMany(Ingredient::class, 'ingredient_meal');
+    }
+
+}
