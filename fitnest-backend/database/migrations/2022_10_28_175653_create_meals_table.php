a078114d94807ea2af8e5c4e771b70e66a825dcd
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 12:30:19 +0200

    backend: adding meals table

---
+++ fitnest-backend/database/migrations/2022_10_28_175653_create_meals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('calories');
+            $table->integer('fats');
+            $table->integer('protein');
+            $table->string('image');
+            $table->string('category');
+            $table->foreignId("user_id")->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+}
