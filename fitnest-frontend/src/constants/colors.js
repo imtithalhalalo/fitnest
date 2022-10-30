8e202c141c28a264b3779faa86b4773860209e78
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 14:32:06 +0200

    frontend: adding constants colors

---
+++ fitnest-frontend/src/constants/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    white: '#fff',
+    black: '#000',
+    green: '#98D8DE',
+    purple: '#867DEA',
+    lightGreen: 'rgba(125, 216, 222, 0.2)',
+    lightPurple: 'rgba(134, 125, 234, 0.2)'
+}
+
+export default colors;
