60c75a4f2822fd4da6eb9eb29a11c49e11300580
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 12:48:09 +0200

    backend: adding programs table

---
+++ fitnest-backend/database/migrations/2022_10_29_194229_create_programs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->integer('num_weeks');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}
