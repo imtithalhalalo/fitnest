1cb1daf53e634e6eacf7e513d9fae58e2000695e
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 12:48:28 +0200

    backend: adding exercises table

---
+++ fitnest-backend/database/migrations/2022_10_29_194245_create_exercises_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->integer('time');
+            $table->longText('description');
+            $table->string('image');
+            $table->binary('is_done')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+}
