3f00a1b145ca6987288c6323b252ffa716b6d319
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 12:27:44 +0200

    backend: adding routes for previous apis

--- fitnest-backend/routes/api.php
+++ fitnest-backend/routes/api.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\MealController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\TrainerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,12 +13,14 @@ Route::group(["middleware"=> "cors"], function(){
     Route::group(["middleware" => "user.role"], function(){
         Route::post('/add_info',[UserController::class, 'addInfo'])->name('add-info');
         Route::post('/update_info',[UserController::class, 'updateInfo'])->name('update-info');
-        Route::post('/upload_image',[UserController::class, 'uploadImage'])->name('upload-image');
         Route::post('/add_water_intake',[UserController::class, 'addWaterIntake'])->name('add-water-intake');
     });
     
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/profile',[AuthController::class, 'profile'])->name('profile');
     Route::post('/update_profile',[AuthController::class, 'updateProfile'])->name('update-profile');
+    Route::post('/upload_image',[UserController::class, 'uploadImage'])->name('upload-image');
+    Route::post('/remove_image', [AuthController::class, 'removeImage'])->name('remove-image');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
