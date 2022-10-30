f2e5f130afa94ae168edc343255a907d5e3cf099
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-30 12:36:06 +0200

    backend: adding addMeal api

--- fitnest-backend/app/Http/Controllers/TrainerController.php
+++ fitnest-backend/app/Http/Controllers/TrainerController.php
@@ -27,5 +27,30 @@ class TrainerController extends Controller {
             'ingredient' => $ingredient
         ],200);
     }
+    
+    //function for user to add meal
+    public function addMeal(Request $request) {
+        $user_id = Auth::user()->id;
+
+        //adding meal info
+        $meal = new Meal;
+        $meal->title = $request->title;
+        $meal->calories = $request->calories;
+        $meal->fats = $request->fats;
+        $meal->protein = $request->protein;
+        $meal->image = $request->image;
+        $meal->category = $request->category;
+        $meal->user_id = $user_id;
+        $meal->save();
+
+        // meal ingredients
+        $ingredients = $request->ingredients;
+        $meal->ingredients()->attach($ingredients);
+
+        return response()->json([
+            'status' => 'success',
+            'meal' => $meal
+        ],200);
+    }
 
 }
