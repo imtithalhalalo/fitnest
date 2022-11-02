994579070bb4775ff1a39f17221e465a345174dd
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-03 00:07:00 +0200

    backend: adding routes

--- fitnest-backend/routes/api.php
+++ fitnest-backend/routes/api.php
@@ -3,25 +3,33 @@
 use App\Http\Controllers\MealController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\TrainerController;
+use App\Models\TrainerInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["middleware"=> "cors"], function(){
 
     Route::group(["middleware" => "user.role"], function(){
-        Route::post('/add_info',[UserController::class, 'addInfo'])->name('add-info');
+        Route::post('/user_info',[UserController::class, 'addInfo'])->name('user-info');
         Route::post('/update_info',[UserController::class, 'updateInfo'])->name('update-info');
         Route::post('/add_water_intake',[UserController::class, 'addWaterIntake'])->name('add-water-intake');
         Route::post('/save_meal',[UserController::class, 'saveMeal'])->name('save-meal');
+        Route::get('/user_info',[UserController::class, 'getInfo'])->name('get-user-info');
     });
 
     Route::group(["middleware" => "trainer.role"], function(){
+        Route::post('/trainer_info',[TrainerController::class, 'addInfo'])->name('trainer-info');
+        Route::post('/update_info',[TrainerController::class, 'updateInfo'])->name('update-info');
         Route::post('/add_ingredient',[TrainerController::class, 'addIngredient'])->name('add-ingredient');
         Route::post('/add_meal',[TrainerController::class, 'addMeal'])->name('add-meal');
+        Route::post('/add_program',[TrainerController::class, 'addProgram'])->name('add-program');
+        Route::post('/add_exercise',[TrainerController::class, 'addExercise'])->name('add-exercise');
         Route::post('/delete_meal/{id?}',[TrainerController::class, 'deleteMeal'])->name('delete-meal');
         Route::post('/delete_ingredient/{id?}',[TrainerController::class, 'deleteIngredient'])->name('delete-ingredient');
+        Route::get('/trainer_info',[TrainerController::class, 'getInfo'])->name('get-trainer-info');
     });    
     
      //meal apis
@@ -34,6 +42,7 @@ Route::group(["middleware"=> "cors"], function(){
     Route::group(['prefix' => 'programs'], function(){
         Route::get('/',[ProgramController::class, 'getPrograms'])->name('get-programs');
         Route::get('/exercise/{id?}',[ProgramController::class, 'getExercisesOfProgram'])->name('get-exercise-of-program');
+        Route::get('/exercises',[ProgramController::class, 'getExercises'])->name('get-exercises');
     }); 
     
     Route::post('/register', [AuthController::class, 'register'])->name('register');
