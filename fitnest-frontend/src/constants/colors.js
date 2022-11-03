9e3e380afaf167d9d7aa1a2bf755e9d7866f113f
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-03 11:01:21 +0200

    frontend: updating colors

--- fitnest-frontend/src/constants/colors.js
+++ fitnest-frontend/src/constants/colors.js
@@ -4,7 +4,9 @@ const colors = {
     green: '#98D8DE',
     purple: '#867DEA',
     lightGreen: 'rgba(125, 216, 222, 0.2)',
-    lightPurple: 'rgba(134, 125, 234, 0.2)'
+    lightPurple: 'rgba(134, 125, 234, 0.2)',
+    lightGrey: 'rgba(154, 154,154, 0.5)',
+    darkGrey: '#676767',
 }
 
 export default colors;
