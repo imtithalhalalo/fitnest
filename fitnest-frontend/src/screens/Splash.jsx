b99dd9487bda31b0f8e1f8acf5c2f388ac8cec20
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-05 21:56:12 +0200

    frontend: modifying splash logo page

--- fitnest-frontend/src/screens/Splash.jsx
+++ fitnest-frontend/src/screens/Splash.jsx
@@ -1,37 +1,40 @@
-import { Text, Image, TouchableOpacity,SafeAreaView, StyleSheet } from 'react-native'
+import { Text, Image, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
 import { style } from '../styles/style';
 import colors from '../constants/colors';
 
 const Splash = ({ navigation }) => {
     return (
-            <SafeAreaView style={styles.container}>
-                <Image source={require('../../assets/images/start.png')} style={style.logo} resizeMode={'contain'} />
-                <TouchableOpacity
-                    style={style.login}
-                    onPress={() =>
-                        navigation.push('SignIn')
-                    }>
-                    <Text style={style.loginText}>Sign In</Text>
-                </TouchableOpacity>
+        <SafeAreaView style={styles.container}>
+            <Image source={require('../../assets/images/start.png')} style={styles.logo} resizeMode={'contain'} />
+            <TouchableOpacity
+                style={style.login}
+                onPress={() =>
+                    navigation.push('SignIn')
+                }>
+                <Text style={style.loginText}>Sign In</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity 
-                    style={style.signup}
-                    onPress={() =>
-                        navigation.push('SignUp')
-                    }>
-                    <Text style={style.signupText}>Sign Up</Text>
-                </TouchableOpacity>
-            </SafeAreaView>
+            <TouchableOpacity
+                style={style.signup}
+                onPress={() =>
+                    navigation.push('SignUp')
+                }>
+                <Text style={style.signupText}>Sign Up</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
 
     )
 }
 export default Splash
 const styles = StyleSheet.create({
-container: {
-    backgroundColor: colors.white,
-    flex: 1,
-    alignItems: 'center',
-    padding: '10%'
-  },
+    container: {
+        backgroundColor: colors.white,
+        flex: 1,
+        alignItems: 'center',
+        padding: '10%'
+    },
+    logo: {
+        width: 300
+    },
 })
