0ca3b226fb41ede6f02b88ba2c5b5d3ff77f3574
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-06 22:45:04 +0200

    frontend: adding style profile for trainer

--- fitnest-frontend/src/screens/trainer/Profile/Profile.jsx
+++ fitnest-frontend/src/screens/trainer/Profile/Profile.jsx
@@ -52,4 +52,52 @@ const Profile = ({ navigation }) => {
 export default Profile
 
 const styles = StyleSheet.create({
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    desc: {
+        fontSize: 16,
+        color: colors.darkGrey,
+        paddingTop: '3%',
+        paddingBottom: '3%',
+        fontWeight: '400'
+    },
+    text: {
+        fontSize: 18,
+        color: colors.black,
+        fontWeight: '500',
+        paddingLeft: '4%'
+    },
+    title: {
+        fontSize: 22,
+        color: colors.black,
+        fontWeight: '500'
+    },
+    name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    },
+    container: {
+        padding: '10%',
+        paddingTop: '30%',
+        backgroundColor: colors.lighterPurple,
+        flex: 1,
+    },
+    profileImg: {
+        width: 120,
+        height: 120,
+        borderRadius: 400 / 2,
+    },
+    profContainer: {
+        display: "flex",
+        alignSelf: "center",
+        marginBottom: 20,
+    },
 })
