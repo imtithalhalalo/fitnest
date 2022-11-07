2f84dc2bced78ed20f8a89cf6de33da2167a008a
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-07 18:14:31 +0200

    frontend: adding style for profile box component

---
+++ fitnest-frontend/src/components/ProfileBox/style.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+import colors from "../../constants/colors";
+
+
+const style = StyleSheet.create({
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    }, 
+    column: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    desc: {
+        fontSize: 16,
+        color: colors.darkGrey,
+        paddingTop: '3%',
+        paddingBottom: '3%',
+        fontWeight: '400'
+    },
+    title: {
+        fontSize: 18,
+        color: colors.black,
+        fontWeight: '500',
+        paddingLeft: '4%'
+    }
+})
+
+export { style }
