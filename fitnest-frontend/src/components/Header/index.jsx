dd2b4d3dc867e5fbc1a11735d15be1cf18418764
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-07 17:36:45 +0200

    frontend: adding header component

---
+++ fitnest-frontend/src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text,Image, View, TouchableOpacity, } from 'react-native'
+import React from 'react'
+import { style } from '../../styles/style'
+import { useNavigation } from '@react-navigation/native'
+import colors from '../../constants/colors';
+import { EvilIcons } from '@expo/vector-icons';
+import { TouchableWithoutFeedback } from '@gorhom/bottom-sheet';
+
+const Header = (props) => {
+
+    const navigation = useNavigation();
+
+    return (
+        <View style={style.bar}>
+            {props.back ? (
+                <TouchableWithoutFeedback onPress={() => {navigation.pop()}}>
+                    <EvilIcons name="arrow-left" size={50} color={colors.purple} />
+                </TouchableWithoutFeedback>
+                
+            ):
+            <></>
+            }
+            <Text style={{ fontSize: 24, fontWeight: '500', marginRight: '10%' }}>{props.text}</Text>
+                
+
+        </View>
+    )
+}
+
+export default Header
