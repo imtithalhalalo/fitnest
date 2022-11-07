f30d5880a9b75a4ab42fd84030af13f5fed19385
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-07 18:20:06 +0200

    frontend: modifying program card component

--- fitnest-frontend/src/components/ProgramCard/index.jsx
+++ fitnest-frontend/src/components/ProgramCard/index.jsx
@@ -8,8 +8,21 @@ const ProgramCard = ({ id, title, image, num_weeks }) => {
     const navigation = useNavigation();
     return (
         <Card style={styles.cardBox}>
+            {image ? (
+                <Card.Cover style={styles.cardCover} source={{ uri: image }} resizeMode={'contain'}/>
+            ) : (
+                <></>
+            )}
             <View style={styles.p4}>
+
                 <Text style={styles.cardTitle}>{title}</Text>
+                {num_weeks ? (
+                    <Text style={styles.cardSubtitle}>{num_weeks} days • {num_weeks} weeks</Text>
+                ) : (
+                    <></>
+                )}
+
+
                 <TouchableOpacity style={[style.secondaryBtn, { width: '40%', alignSelf: 'flex-end', marginTop: 10, marginBottom: 10 }]} onPress={() => { navigation.navigate('WorkoutSteps', { program_id: id }) }}>
                     <Text style={[style.secondaryText, { fontSize: 18, paddingLeft: '3%', paddingRight: '3%' }]}>View Steps</Text>
                 </TouchableOpacity>
