ad34ebc910f84f45644a7cdd6bfd475f6f389302
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-07 17:33:34 +0200

    frontend: adding style for loader component

---
+++ fitnest-frontend/src/components/Loader/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    loading: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+})
+
+export default styles
