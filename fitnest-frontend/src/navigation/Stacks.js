67686f404d29be221defd47ddeba9af12141766b
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-08 23:49:19 +0200

    frontend: adding trainer stack for navigation

--- fitnest-frontend/src/navigation/Stacks.js
+++ fitnest-frontend/src/navigation/Stacks.js
@@ -2,6 +2,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
 import Splash from "../screens/Splash";
+import BottomTabs from "./UserTabs";
+import BottomTabsTrainer from "./TrainerTabs";
+import AddMeal from "../screens/trainer/AddMeal";
+import AddPlan from "../screens/trainer/AddPlan";
+import AddStep from "../screens/trainer/AddStep";
+import Steps from "../screens/user/UserInfo/Steps";
+import ConnectExerciseToProgram from "../screens/trainer/ConnectSteps";
 
 const Stack = createStackNavigator();
 const Stacks = () => {
@@ -10,6 +17,10 @@ const Stacks = () => {
           <Stack.Screen name="Splash" component={Splash} />
           <Stack.Screen name="SignIn" component={SignIn} />
           <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="AddPlan" component={AddPlan} />
+          <Stack.Screen name="AddStep" component={AddStep} />
+          <Stack.Screen name="ConnectExerciseToProgram" component={ConnectExerciseToProgram} />
+          <Stack.Screen name="AddMeal" component={AddMeal} />
         </Stack.Navigator>
     );
 }
