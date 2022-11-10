7ab33f22221423d932e6c85987ffe446c09b4f70
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-10 18:07:05 +0200

    frontend: adding navigation container to the app

--- fitnest-frontend/App.js
+++ fitnest-frontend/App.js
@@ -1,19 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Stacks from './src/navigation/Stacks';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
+    <>
+        <NavigationContainer>
+          <Stacks />
+        </NavigationContainer>
+    </>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
