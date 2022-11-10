516b140dd8c7fb4dca269e4d744f9a10f405706d
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-10 17:36:05 +0200

    frontend: adding meal stack and plan to tabs

--- fitnest-frontend/src/navigation/UserTabs.js
+++ fitnest-frontend/src/navigation/UserTabs.js
@@ -1,7 +1,12 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Plan from "../screens/user";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import colors from "../constants/colors";
 import HomeStack from "./HomeStack";
+import MealStack from "./MealStack";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "react-native-vector-icons";
+import UserProfileStack from "./ProfileStack/UserProfileStack";
 import UserChatStack from "./ChatStack/UserChatStack";
 
 const BottomTabs = () => {
@@ -37,6 +42,33 @@ const BottomTabs = () => {
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons
                 name="message"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <BottomTabsNav.Screen
+          name="Plan"
+          component={Plan}
+          options={{
+            tabBarLabel: "Progress",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons 
+                name="stats-chart"
+                size={size} 
+                color={color} />
+            ),
+          }}
+        />
+        <BottomTabsNav.Screen
+          name="MealStack"
+          component={MealStack}
+          options={{
+            tabBarLabel: "Meals",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="heart"
                 color={color}
                 size={size}
               />
