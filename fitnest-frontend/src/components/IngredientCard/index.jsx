72d80707a2e4ca86f3be30c58fb6d04cf407a56e
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-11 15:47:22 +0200

    frontend: adding green ingredient card

--- fitnest-frontend/src/components/IngredientCard/index.jsx
+++ fitnest-frontend/src/components/IngredientCard/index.jsx
@@ -7,14 +7,19 @@ const IngredientCard = (props) => {
 
     return (
         <>
-
+        {props.green ? (
+            <View style={styles.ingredientGreenCard}>
+                    <Text style={styles.ingredientGreenText}>{props.title}</Text>
+            </View>
+        ) : (
             <View style={styles.ingredientCard}>
                 <View style={styles.row}>
                     <Text style={styles.ingredientText}>{props.title}</Text>
-                    <FontAwesome name="times" size={20} color={colors.purple} style={styles.removeIcon} />
+                    <FontAwesome name="times" size={20} color={colors.purple} style={styles.removeIcon}/>
                 </View>
             </View>
-
+        )}
+            
 
         </>
 
