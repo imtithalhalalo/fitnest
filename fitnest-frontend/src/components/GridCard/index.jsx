522ce692e0d4e2a59bf8fde2294ea5bbb2c63e1e
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-11 14:18:42 +0200

    frontend: adding grid card component

---
+++ fitnest-frontend/src/components/GridCard/index.jsx
@@ -0,0 +1,27 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+import { Card } from 'react-native-paper';
+import colors from '../../constants/colors'
+import { Ionicons } from "react-native-vector-icons";
+
+const GridCard = (props) => {
+    return (
+        <Card style={[styles.gridItem, { backgroundColor: props.color, shadowColor: props.shadowColor }]}
+            onPress={props.onPress} >
+            <View style={styles.row}>
+                <Text style={styles.titleLabel}>{props.title}</Text>
+                <Image source={props.image} style={{ width: 28, height: 28 }} />
+            </View>
+            <View style={[styles.row, { paddingTop: '30%' }]}>
+                <Ionicons
+                    name="stats-chart"
+                    size={40}
+                    color={colors.green} />
+                <Text style={{ color: colors.purple }}>{props.water} Cups</Text>
+            </View>
+        </Card>
+    )
+}
+
+export default GridCard
