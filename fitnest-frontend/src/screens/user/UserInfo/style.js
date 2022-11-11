ab857b631eb3b62e49c5dc43eee771e6b4947960
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-11 12:29:52 +0200

    frontend: adding style for more steps user info

--- fitnest-frontend/src/screens/user/UserInfo/style.js
+++ fitnest-frontend/src/screens/user/UserInfo/style.js
@@ -24,6 +24,39 @@ const styles = StyleSheet.create({
         height: 40,
         fontSize: 20,
     },
+    inputSmall: {
+        borderWidth: 1,
+        borderColor: colors.black,
+        borderRadius: 15,
+        padding: 10,
+        width: '100%',
+    },
+    inputContainer: {
+        marginVertical: 1,
+        width: '80%',
+        marginBottom: '5%'
+    },
+    map: {
+        width: '100%',
+        height: 500,
+        margin: 20,
+        alignSelf: 'center',
+    },
+    nextBtnTextStyle: {
+        color: colors.white,
+        fontSize: 18
+    },
+    nextBtnStyle: {
+        backgroundColor: colors.purple,
+        borderRadius: 15,
+        width: '100%',
+        paddingLeft: 40,
+        paddingRight: 40
+    },
+    question: {
+        textAlign: 'center',
+        fontSize: 20
+    },
 })
 
 export default styles
