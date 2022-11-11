0c07abb3505b68a57a93cce8b0d77f6bf27a769b
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-11 11:37:04 +0200

    frontend: adding style for connect steps

--- fitnest-frontend/src/screens/trainer/ConnectSteps.jsx
+++ fitnest-frontend/src/screens/trainer/ConnectSteps.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Text, View, SafeAreaView, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TouchableWithoutFeedback } from 'react-native'
 import colors from '../../constants/colors'
 import { style } from '../../styles/style'
 import { EvilIcons } from '@expo/vector-icons';
@@ -15,8 +15,8 @@ const ConnectExerciseToProgram = ({ navigation }) => {
 
   return (
     <>
-      <SafeAreaView>
-        <View >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.bar}>
           <TouchableWithoutFeedback onPress={() => { navigation.pop() }}>
             <EvilIcons name="arrow-left" size={50} color={colors.purple} />
           </TouchableWithoutFeedback>
@@ -27,6 +27,10 @@ const ConnectExerciseToProgram = ({ navigation }) => {
           <Text style={[{ textAlign: 'center', fontSize: 20 }]}>Choose Plan</Text>
           <View style={[style.inputContainer, { alignItems: "center" }]}>
             <Dropdown
+              style={styles.dropdown}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
               data={programs}
               placeholder="Choose Plan"
               labelField="title"
@@ -37,6 +41,10 @@ const ConnectExerciseToProgram = ({ navigation }) => {
           <Text style={[{ textAlign: 'center', fontSize: 20, marginTop: '5%' }]}>Choose Exercise</Text>
           <View style={[style.inputContainer, { alignItems: "center" }]}>
             <Dropdown
+              style={styles.dropdown}
+              placeholderStyle={styles.placeholderStyle}
+              selectedTextStyle={styles.selectedTextStyle}
+              inputSearchStyle={styles.inputSearchStyle}
               data={exercises}
               placeholder="Choose Exercise"
               labelField="title"
@@ -57,3 +65,45 @@ const ConnectExerciseToProgram = ({ navigation }) => {
 }
 
 export default ConnectExerciseToProgram;
+
+const styles = StyleSheet.create({
+  bar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: '10%'
+  },
+  container: {
+    backgroundColor: colors.white,
+    flex: 1,
+    alignItems: 'flex-start',
+    padding: '10%'
+  },
+  textStyle: {
+    color: colors.white,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  dropdown: {
+    borderWidth: 1,
+    borderColor: colors.black,
+    borderRadius: 15,
+    padding: 10,
+    width: 330,
+  },
+  placeholderStyle: {
+    fontSize: 18,
+  },
+  selectedTextStyle: {
+    fontSize: 18,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 20,
+  },
+  inputContainer: {
+    marginVertical: 1,
+    width: '80%',
+    marginBottom: '5%'
+  },
+})
