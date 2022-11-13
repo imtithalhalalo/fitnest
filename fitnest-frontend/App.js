c2d8565499503c3e55a0935c53b1e25548851b8a
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-13 23:32:08 +0200

    frontend: adding provider to app

--- fitnest-frontend/App.js
+++ fitnest-frontend/App.js
@@ -1,14 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Stacks from './src/navigation/Stacks';
+import UserProvider from './src/stores/UserContext';
 
 
 export default function App() {
 
   return (
     <>
+      <UserProvider>
         <NavigationContainer>
           <Stacks />
         </NavigationContainer>
+      </UserProvider>
     </>
     
   );
