8aa5ff1863cbd92176bb7b9682aff8d381a2402e
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-13 23:40:57 +0200

    frontend: modifying splash screen

--- fitnest-frontend/src/screens/Splash.jsx
+++ fitnest-frontend/src/screens/Splash.jsx
@@ -2,39 +2,32 @@ import { Text, Image, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-n
 import React from 'react'
 import { style } from '../styles/style';
 import colors from '../constants/colors';
+import Button from '../components/Button';
 
 const Splash = ({ navigation }) => {
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={style.mainContainer}>
             <Image source={require('../../assets/images/start.png')} style={styles.logo} resizeMode={'contain'} />
+            <Button text={"Sign Up"} onPress={() =>
+                navigation.push('SignUp')
+            } />
+
             <TouchableOpacity
                 style={style.login}
                 onPress={() =>
                     navigation.push('SignIn')
                 }>
-                <Text style={style.loginText}>Sign In</Text>
+                <Text style={style.loginText}>Log In</Text>
             </TouchableOpacity>
-
-            <TouchableOpacity
-                style={style.signup}
-                onPress={() =>
-                    navigation.push('SignUp')
-                }>
-                <Text style={style.signupText}>Sign Up</Text>
-            </TouchableOpacity>
+            
         </SafeAreaView>
 
     )
 }
 export default Splash
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: colors.white,
-        flex: 1,
-        alignItems: 'center',
-        padding: '10%'
-    },
     logo: {
-        width: 300
+        width: '80%',
+        alignSelf: 'center'
     },
 })
