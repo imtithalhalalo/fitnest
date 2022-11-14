7f3ab151e5e9762fe540fe582ece4dddeb68d023
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-14 23:57:55 +0200

    frontend: adding axios get steps of program

--- fitnest-frontend/src/screens/user/Programs/WorkoutSteps/index.jsx
+++ fitnest-frontend/src/screens/user/Programs/WorkoutSteps/index.jsx
@@ -1,13 +1,38 @@
 import { SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import Header from '../../../../components/Header';
+import { BASE_URL } from '../../../../variables/global';
+import axios from 'axios';
 import { style } from '../../../../styles/style';
 
 const WorkoutSteps = ({ route }) => {
+  const { program_id, program_name } = route.params;
+  const [loading, setLoading] = useState(true)
 
+  const [steps, setSteps] = useState([])
+
+  useLayoutEffect( () => {
+
+    const getSteps = async () => {
+
+      await axios({
+        method: 'GET',
+        url: `${BASE_URL}/programs/exercise/${program_id}`,
+        headers: { 'Content-Type': 'multipart/form-data;' },
+      }).then(response => {
+        setSteps(response.data['exercise']);
+        console.log(response.data)
+        setLoading(false)
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+    getSteps()
+
+  }, [])
   return (
     <SafeAreaView style={style.mainContainer}>
-      <Header text={"Workout Steps"} />
+      <Header text={ program_name + " Exercises"} back={"back"}/>
 
     </SafeAreaView>
   )
