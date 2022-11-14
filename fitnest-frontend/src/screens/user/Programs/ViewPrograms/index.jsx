3e587a4cf6b2cda760fd038c2d6fec2b8722eef6
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-14 23:59:20 +0200

    frontend: adding axios get programs

--- fitnest-frontend/src/screens/user/Programs/ViewPrograms/index.jsx
+++ fitnest-frontend/src/screens/user/Programs/ViewPrograms/index.jsx
@@ -1,15 +1,44 @@
-import { SafeAreaView } from 'react-native'
-import React from 'react'
+import { FlatList, SafeAreaView } from 'react-native'
+import React, { useState, useLayoutEffect, useEffect, useMemo, useContext } from 'react'
 import Header from '../../../../components/Header';
 import Search from '../../../../components/Search';
+import ProgramCard from '../../../../components/ProgramCard';
 import { style } from '../../../../styles/style';
+import EmptyState from '../../../../components/EmptyState';
+import useGetPrograms from '../../../../services/user/getPrograms';
+import Loader from '../../../../components/Loader';
+import { UserContext } from '../../../../stores/UserContext';
+import { BASE_URL } from '../../../../variables/global';
+import axios from 'axios';
 
 const Programs = () => {
+  const [search, setSearch] = useState('');
+  const [filteredPrograms, setFilteredPrograms] = useState([])
+  const { userData } = useContext(UserContext);
+  const [programs, setPrograms] = useState([])
+  const [loading, setloading] = useState(true);
+  const getPrograms = () => {
+    axios({
+      method: 'GET',
+      url: `${BASE_URL}/programs/`,
+    }).then(response => {
+      console.log(response.data['programs'])
+      setFilteredPrograms(response.data['programs'])
+      setPrograms(response.data['programs'])
+      setloading(false)
+    }).catch((err) => {
+      setError(err)
+    })
+  };
+
+  useEffect(() => {
+    getPrograms();
+  }, []);
+
   return (
     <SafeAreaView style={style.mainContainer}>
-      <Header text={"My Plans"} />
+      <Header text={"My Plans"} image={userData.image} />
       <Search />
-      
     </SafeAreaView>
   )
 }
