7e5d6d725f1154669eab89f142aad55d3ddf9576
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-14 23:55:08 +0200

    frontend: edit grid card component

--- fitnest-frontend/src/components/GridCard/index.jsx
+++ fitnest-frontend/src/components/GridCard/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { styles } from './style'
 import { Card } from 'react-native-paper';
@@ -7,30 +7,34 @@ import { Ionicons } from "react-native-vector-icons";
 
 const GridCard = (props) => {
     return (
-        <Card style={[styles.gridItem, { backgroundColor: props.color }]}
-            onPress={props.onPress} >
-            <View style={styles.row}>
-                <Text style={styles.titleLabel}>{props.title}</Text>
-                <Image source={props.image} style={{ width: 28, height: 28 }} />
-            </View>
 
-            {
-                props.sleeping ? (
-                    <View style={[styles.column, { paddingTop: '30%' }]}>
-                        <Text style={styles.subtitle}>{props.hours}</Text>
-                        <Text style={styles.subtitle}>Hours</Text>
-                    </View>
-                ) : (
-                    <View style={[styles.row, { paddingTop: '30%' }]}>
-                        <Ionicons
-                            name="stats-chart"
-                            size={40}
-                            color={colors.green} />
-                        <Text style={styles.subtitle}>{props.water} Cups</Text>
-                    </View>
-                )
-            }
+        <Card style={[styles.gridItem, { backgroundColor: props.color }]}
+             >
+            <TouchableOpacity style={{ width: '100%' }} onPress={props.onPress}>
+                <View style={styles.row}>
+                    <Text style={styles.titleLabel}>{props.title}</Text>
+                    <Image source={props.image} style={{ width: 28, height: 28 }} />
+                </View>
+
+                {
+                    props.sleeping ? (
+                        <View style={[styles.column, { paddingTop: '40%' }]}>
+                            <Text style={styles.subtitle}>{props.hours}</Text>
+                        </View>
+                    ) : (
+                        <View style={[styles.row, { paddingTop: '30%' }]}>
+                            <Ionicons
+                                name="stats-chart"
+                                size={40}
+                                color={colors.green} />
+                            <Text style={styles.subtitle}>{props.water} Cups</Text>
+                        </View>
+                    )
+                }
+                </TouchableOpacity>
         </Card>
+
+
     )
 }
 
