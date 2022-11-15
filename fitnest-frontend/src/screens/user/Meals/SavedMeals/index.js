169eb07e6731800a0148eeee68c05d0d5afd74c1
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-15 12:18:08 +0200

    frontend: rendering saved meals

--- fitnest-frontend/src/screens/user/Meals/SavedMeals/index.js
+++ fitnest-frontend/src/screens/user/Meals/SavedMeals/index.js
@@ -1,13 +1,39 @@
-import { SafeAreaView, Text } from 'react-native'
+import { FlatList, SafeAreaView } from 'react-native'
 import React from 'react'
+import useSavedMeals from '../../../../services/user/getSavedMeals';
+import SavedMealCard from '../../../../components/SavedMealCard';
+import EmptyState from '../../../../components/EmptyState';
 import Header from '../../../../components/Header';
 import { style } from '../../../../styles/style';
 
 const SavedMeals = () => {
 
+    const { response, loading, error } = useSavedMeals();
+
+    const renderItem = ({ item }) => (
+        <SavedMealCard
+            title={item.title}
+            image={item.image}
+            id={item.id}
+        />
+    )
     return (
         <SafeAreaView style={style.mainContainer}>
             <Header text={"My Saved Meals"} back="back" />
+
+
+          {
+            response.length == 0 ? (
+            <EmptyState image={require("../../../../../assets/images/healthyfood.png")} description={"This section will contain your saved healthy meals "} title={"No saved meals"} />
+            ) :
+            <FlatList
+                data={response}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                contentContainerStyle={{ paddingBottom: 50 }}
+                ListFooterComponent={<SafeAreaView />}
+            />
+          } 
 
         </SafeAreaView>
     )
