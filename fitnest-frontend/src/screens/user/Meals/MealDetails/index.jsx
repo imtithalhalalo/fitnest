3162acec3919ef4791d6f230231efb8a03d15527
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-15 12:02:55 +0200

    frontend: adding ingredients in meals page

--- fitnest-frontend/src/screens/user/Meals/MealDetails/index.jsx
+++ fitnest-frontend/src/screens/user/Meals/MealDetails/index.jsx
@@ -1,39 +1,71 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, Image, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import React, { useState, useLayoutEffect } from 'react'
 import { styles } from './style';
 import Header from '../../../../components/Header';
+import useGetMeal from '../../../../services/user/getMealById';
+import useGetMealIngredients from '../../../../services/user/getMealIngredients';
+import IngredientCard from '../../../../components/IngredientCard';
 import { style } from '../../../../styles/style';
 import Button from '../../../../components/Button';
 
-const MealDetails = () => {
-
+const MealDetails = ({ route }) => {
+  const { id, title, image } = route.params;
+  const [meal, setMeal] = useState({});
+  const [ingredients, setIngredients] = useState([]);
+  const { response, loading, error } = useGetMeal({ id });
+  const { res, load, err } = useGetMealIngredients({ id });
+  useLayoutEffect(() => {
+    if (response !== null) {
+      setMeal(response);
+    }
+    if (res !== null) {
+      setIngredients(res);
+    }
+  }, [response, res]);
 
   return (
     <SafeAreaView style={style.mainContainer}>
-
-      <Header text={"Meal"} back="back"/>
+    <TouchableOpacity onPress={viewSaved}>
+      <View style={[style.row, {alignSelf: 'flex-end'}]}>
+      <Text style={styles.txt}>Saved Meals</Text>
+      <Image source={ require('../../../../../assets/images/bookmark.png')} style={{ width: 25, height: 25, alignSelf: 'flex-end', marginRight: '2%' }} />
+      </View>
+      </TouchableOpacity>
+      <Header text={title} back="back"/>
+      
+      
+      {meal.image ? (
+        <Image source={{ uri: meal.image }} style={styles.img} resizeMode={'contain'}/>
+      ) : <></>
+      }
       <ScrollView>
         <View style={styles.row}>
           <View style={styles.column}>
             <Text style={styles.title}>Calories</Text>
-            <Text style={styles.value}>33234  kcal</Text>
+            <Text style={styles.value}>{meal.calories}  kcal</Text>
           </View>
           <View style={styles.column}>
             <Text style={styles.title}>Fats</Text>
-            <Text style={styles.value}>44 g</Text>
+            <Text style={styles.value}>{meal.fats} g</Text>
           </View>
           <View style={styles.column}>
             <Text style={styles.title}>Protein</Text>
-            <Text style={styles.value}>5 g</Text>
+            <Text style={styles.value}>{meal.protein} g</Text>
           </View>
         </View>
         <View style={styles.dash}></View>
 
         <View style={styles.ingredientsContainer}>
-          
+          {
+            ingredients.map((i =>
+              <IngredientCard
+                title={i.ingredient}
+                key={i.id}
+                green={true} />))
+          }
         </View>
 
-        <Button text={"Save"} />
+        <Button text={"Save"} onPress={handleSave}/>
       </ScrollView>
     </SafeAreaView>
   )
