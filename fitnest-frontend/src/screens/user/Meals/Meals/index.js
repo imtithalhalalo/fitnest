4b50b1bfaffadab44b75668eb7cd9272b07158c9
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-15 11:54:34 +0200

    frontend: adding setMeals in meals screen

--- fitnest-frontend/src/screens/user/Meals/Meals/index.js
+++ fitnest-frontend/src/screens/user/Meals/Meals/index.js
@@ -1,15 +1,33 @@
 import { SafeAreaView, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import colors from '../../../../constants/colors';
 import Header from '../../../../components/Header';
 import Search from '../../../../components/Search';
 import ButtonToggleGroup from 'react-native-button-toggle-group';
+import MealCard from '../../../../components/MealCard';
+import useAxios from '../../../../services/user/getMeals';
 import { style } from '../../../../styles/style';
 
 const Meals = () => {
     
+    const [meals, setMeals] = useState([]);
     const [category, setCategory] = useState('Breakfast');
+    const { response, loading, error } = useAxios({category});
+    const [search, setSearch] = useState('');
 
+    useEffect(() => {
+        if (response !== null) {
+            setMeals(response);
+        }
+    }, []);
+
+    const renderItem = ({ item }) => (
+        <MealCard
+            title={item.title}
+            image={item.image}
+            id={item.id}
+        />
+    )
     return (
         <SafeAreaView style={style.mainContainer}>
             <Header text={"My Suggested Meals"} />
