7b6c608746bee86dffe43181e4a051182924c9a4
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-15 14:40:03 +0200

    frontend: adding getLastIngredient id

--- fitnest-frontend/src/screens/trainer/AddMeal/index.jsx
+++ fitnest-frontend/src/screens/trainer/AddMeal/index.jsx
@@ -1,10 +1,14 @@
-import { Text, View, SafeAreaView, ScrollView } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { Text, View, SafeAreaView, ScrollView, Alert } from 'react-native'
+import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { Dropdown } from 'react-native-element-dropdown';
 import { style } from '../../../styles/style';
 import * as ImagePicker from "expo-image-picker";
 import colors from '../../../constants/colors';
+import axios from 'axios';
+import { BASE_URL } from '../../../variables/global';
 import addIngredient from '../../../services/trainer/addIngredient';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import IngredientCard from '../../../components/IngredientCard';
 import { FontAwesome } from "@expo/vector-icons";
 import Button from '../../../components/Button';
 import Header from '../../../components/Header';
@@ -31,6 +35,28 @@ const AddMeal = () => {
   const [error, setError] = useState('')
   const [modalVisibility, setModalVisibility] = useState(false);
 
+
+  let res = [];
+  useLayoutEffect(() => {
+
+    const getLastId = async () => {
+
+      await axios({
+        method: 'GET',
+        url: `${BASE_URL}/meals/last_ingredient`,
+        headers: { 'Content-Type': 'multipart/form-data;' },
+      }).then(response => {
+        setId(response.data.id)
+        console.log(response.data.id)
+      }).catch((err) => {
+
+        console.log(err);
+
+      })
+    }
+    getLastId()
+
+  }, [])
 
   const uploadImage = async () => {
     let res = await ImagePicker.launchImageLibraryAsync({
