223a4a18ea6440dbc854f875fe27ac38ea0ccb80
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-15 15:12:13 +0200

    frontend: getting all trainers api

--- fitnest-frontend/src/screens/user/Chat/Chats.jsx
+++ fitnest-frontend/src/screens/user/Chat/Chats.jsx
@@ -1,16 +1,37 @@
-import { SafeAreaView, FlatList } from 'react-native'
-import React, { useState, useContext } from 'react'
+import { SafeAreaView, Text, FlatList } from 'react-native'
+import React, { useLayoutEffect, useState, useContext } from 'react'
 import Header from '../../../components/Header';
 import ChatCard from '../../../components/ChatCard';
+import { BASE_URL } from '../../../variables/global';
+import axios from 'axios'
 import EmptyState from '../../../components/EmptyState';
 import { UserContext } from '../../../stores/UserContext';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import { style } from '../../../styles/style';
 import Loader from '../../../components/Loader';
 
 const Chats = ({ navigation }) => {
     const [trainers, setTrainers] = useState([])
     const [loading, setLoading] = useState(true)
-    
+    const { userData } = useContext(UserContext);
+
+    useLayoutEffect(() => {
+        const getTrainer = async () => {
+                await axios({
+                    method: 'GET',
+                    url: `${BASE_URL}/chat/trainer`,
+                    headers: { 'Content-Type': 'multipart/form-data;' },
+                }).then(response => {
+                    setTrainers(response.data['trainers'])
+                    console.log(response.data['trainers'])
+                    setLoading(false)
+                }).catch((err) => {
+                    console.log(err);
+                })
+        }
+        getTrainer();
+    }, [])
+
     return (
         <SafeAreaView style={style.mainContainer}>
             <Header text={"Chat With Trainer"} />
