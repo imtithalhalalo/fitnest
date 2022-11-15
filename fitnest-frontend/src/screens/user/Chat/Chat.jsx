4fa87e6113b79947e47c7afc924c8b3b48363015
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-15 15:14:34 +0200

    frontend: adding setMessages chat on start

--- fitnest-frontend/src/screens/user/Chat/Chat.jsx
+++ fitnest-frontend/src/screens/user/Chat/Chat.jsx
@@ -1,7 +1,16 @@
 import { View, SafeAreaView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import Header from '../../../components/Header'
 import { GiftedChat } from 'react-native-gifted-chat'
+import { database } from '../../../../firebase'
+import {
+  collection,
+  orderBy,
+  onSnapshot,
+  query,
+  addDoc,
+  doc,
+} from "firebase/firestore";
 import renderBubble from '../../../helpers/renderBubble'
 import renderSend from '../../../helpers/renderSend'
 import renderInputToolbar from '../../../helpers/renderInputToolbar'
@@ -11,6 +20,24 @@ const Chat = ({ route }) => {
   
   const { chatID, trainer_id, name, image } = route.params;
   const [messages, setMessages] = useState([]);
+  
+  useEffect(() => {
+    const collectionRef = collection(doc(database, "chats", chatID), "messages");
+    const q = query(collectionRef, orderBy("createdAt", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setMessages(
+        snapshot.docs.map((doc) => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+
+        }))
+      )
+    })
+    return unsubscribe
+  }, [])
+
 
   return (
     <SafeAreaView style={style.mainContainer}>
