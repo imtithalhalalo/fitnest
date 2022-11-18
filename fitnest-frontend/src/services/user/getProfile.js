45fe2fff7169ddae023cceaed003626a6deaac5a
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-18 12:56:53 +0200

    frontend: adding get profile axios

---
+++ fitnest-frontend/src/services/user/getProfile.js
@@ -0,0 +1,29 @@
+import { useState, useLayoutEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../variables/global';
+
+const useAxios = () => {
+    const [res, setResponse] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setloading] = useState(true);
+    
+    const getProfile = () => {
+        axios({
+            method: 'GET',
+            url: `${BASE_URL}/profile`,
+        }).then(response => {
+            setResponse(response.data['user'])
+            console.log(response.data['user'])
+            setloading(false)
+        }).catch((err) => {
+            setError(err)
+        })
+    }
+    useLayoutEffect(() => {
+        getProfile();
+    }, [res, error, loading]);
+
+    return { res, error, loading };
+};
+
+export default useAxios;
