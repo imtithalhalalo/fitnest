b685ec8701afdec40bb0f4085f5b7fcb1818674d
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-18 11:14:09 +0200

    frontend: adding graph water consumption

---
+++ fitnest-frontend/src/screens/user/Progress/WaterConsumption/index.jsx
@@ -0,0 +1,72 @@
+import { Dimensions, SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import Button from '../../../../components/Button'
+import {
+  BarChart,
+} from "react-native-chart-kit";
+import colors from '../../../../constants/colors';
+import { style } from '../../../../styles/style';
+import Header from '../../../../components/Header';
+import styles from './style';
+import { AntDesign } from "@expo/vector-icons";
+
+
+const WaterConsumption = ({ navigation }) => {
+
+  let data = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [
+      {
+        data: [60, 90, 70, 50, 80]
+      }
+    ]
+  };
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{ marginLeft: '5%' }}>
+          <Header text={"Water Consumption"} back={"back"} />
+        </View>
+
+        <View style={{ marginTop: '20%' }}>
+          <TouchableOpacity>
+            <Text style={[style.secondaryText, { marginBottom: '10%' }]}><AntDesign name="plus" size={18} color={colors.purple} style={{ marginRight: '5%', marginBottom: '10%', }} /> Add Water Amount</Text>
+          </TouchableOpacity>
+
+          <BarChart
+            data={data}
+            height={300}
+            verticalLabelRotation={30}
+            style={{ backgroundColor: colors.white, borderRadius: 15 }}
+            width={Dimensions.get('window').width}
+            chartConfig={{
+              backgroundColor: colors.purple,
+              backgroundGradientFrom: colors.purple,
+              backgroundGradientTo: colors.purple,
+              decimalPlaces: 0,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                stroke: colors.white
+              }
+            }}
+          />
+        </View>
+
+        <View style={[style.mainContainer, { marginTop: '10%' }]}>
+          <Button text={"Go Back"} onPress={() => { navigation.pop() }} />
+        </View>
+      </SafeAreaView>
+
+
+
+    </>
+
+
+  )
+}
+
+export default WaterConsumption
