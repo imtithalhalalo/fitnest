29b20d946236fa4d4611639153d4f174285bddbf
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-18 11:40:22 +0200

    frontend: adding modal for sleeping card

--- fitnest-frontend/src/screens/user/Progress/Sleeping/index.js
+++ fitnest-frontend/src/screens/user/Progress/Sleeping/index.js
@@ -1,9 +1,23 @@
+import { View, Text, Modal, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import { style } from '../../../../styles/style'
 import colors from '../../../../constants/colors'
 import GridCard from '../../../../components/GridCard'
+import styles from './style'
+import { FontAwesome } from "@expo/vector-icons";
 
 const Sleeping = () => {
     const [modalVisibility, setModalVisibility] = useState(false);
+
+
+    const [wokeUpHour, setWokeUpHour] = useState(0);
+    const [wokeUpMin, setWokeUpMin] = useState(0);
+
+    const [sleepHour, setSleepHour] = useState(0);
+    const [sleepMin, setSleepMin] = useState(0);
+
+    const [HoursDuration, setHoursDuration] = useState(0);
+    const [MinDuration, setMinDuration] = useState(0);
     const [duration , setDuration] = useState('');
 
     return (
@@ -18,6 +32,51 @@ const Sleeping = () => {
                 hours={duration}
                 onPress={() => setModalVisibility(true)}
             />
+            {/* Sleeping Duration card */}
+            {
+                modalVisibility ?
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisibility}
+                        onRequestClose={() => {
+                            setModalVisibility(!modalVisibility);
+                        }}
+                    >
+                        <View style={styles.centered}>
+                            <View style={styles.modal}>
+                                <TouchableOpacity style={{ alignItems: 'flex-end', marginBottom: 20 }}>
+                                    <FontAwesome name={"close"} size={30} color={colors.purple} />
+                                </TouchableOpacity>
+
+                                <View style={[styles.row, styles.btnHoursContainer]}>
+                                    <Text style={style.inputLabel}>Went to bed</Text>
+                                    <TouchableOpacity>
+                                        <Text style={styles.hoursBtn}>{`${sleepHour}h ${sleepMin}m`}</Text>
+                                    </TouchableOpacity>
+                                </View>
+
+                                <View style={[styles.row, styles.btnHoursContainer]}>
+                                    <Text style={style.inputLabel}>Woke Up</Text>
+                                    <TouchableOpacity>
+                                        <Text style={styles.hoursBtn}>{`${wokeUpHour}h ${wokeUpMin}m`}</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <TouchableOpacity>
+                                    <Text style={styles.btnLabel}>Get Duration </Text>
+                                </TouchableOpacity>
+
+                                
+                                <Text style={[styles.average]}>{`${HoursDuration}h ${MinDuration}m`}</Text>
+                                <TouchableOpacity style={style.primaryBtn}>
+                                    <Text style={style.primaryTextBtn}>Add</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+
+                    </Modal> : <></>
+            }
         </>
 
     )
