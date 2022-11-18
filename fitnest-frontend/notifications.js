5e4805763b3209e8331c2cd008ccd7ef1760fd5f
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-18 12:55:38 +0200

    frontend: adding register for push notification

--- fitnest-frontend/notifications.js
+++ fitnest-frontend/notifications.js
@@ -48,5 +48,37 @@ const PushNotificationsComponent = ({ setExpoPushToken }) => {
     return <></>;
 }
 
+async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+        const { status: existingStatus } =
+            await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+            alert("Failed to get push token for push notification!");
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        
+        await AsyncStorage.setItem('push_token', token)
+    } else {
+        alert("Must use physical device for Push Notifications");
+    }
+
+    if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+            name: "default",
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: "#FF231F7C",
+        });
+    }
+
+    return token;
+}
 
 export default PushNotificationsComponent
