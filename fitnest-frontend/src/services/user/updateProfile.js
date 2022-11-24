26adac597b88291e1d76a298f8ee5475b7da95d8
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-25 00:17:24 +0200

    frontend: modifying updateProfile image

--- fitnest-frontend/src/services/user/updateProfile.js
+++ fitnest-frontend/src/services/user/updateProfile.js
@@ -4,28 +4,26 @@ import { Alert } from 'react-native';
 import { BASE_URL } from '../../variables/global';
 
 const updateProfile = async (user) => {
-
     const data = {
         name: user.name,
         email: user.email,
         phone_num: user.phone_num,
-        image: user.image
+        image: user.image,
+        ext: user.ext
     };
     const token = await AsyncStorage.getItem('token')
-    await axios({
+    try {
+        const res = await axios({
             headers:{'Authorization':'Bearer '+ token},
             method:'POST',
             url: `${BASE_URL}/update_profile`,
             data:data,
-    })
-    .then(function (response) {
+        })
         Alert.alert('Updated')
-        return true
-    })
-    .catch(function (error) {
-        console.log(error)
-        return false
-    });
+        return {success: true, data: res.data['user']}
+    }catch (error) {
+        return {success: false, error: error}
+    }
 }
 
 export default updateProfile;
