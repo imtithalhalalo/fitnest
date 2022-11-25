6843c112324b7a0aae6b30f7b57dd5fb23bdd970
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-25 15:35:21 +0200

    frontend: modifying image in savedMeals card

--- fitnest-frontend/src/components/SavedMealCard/index.jsx
+++ fitnest-frontend/src/components/SavedMealCard/index.jsx
@@ -2,14 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Card } from 'react-native-paper';
 import { styles } from './style';
-
+import { address } from '../../variables/global';
 const SavedMealCard = (props) => {
     return (
         <Card style={styles.cardBox}>
             <View style={styles.p4}>
                 <View style={styles.box}>
                     {props.image ? (
-                        <Card.Cover style={styles.cardCover} source={{ uri: props.image }} />
+                        <Card.Cover style={styles.cardCover} source={{ uri: `${address}/${props.image}` }} />
                     ) : (
                         <></>
                     )}
