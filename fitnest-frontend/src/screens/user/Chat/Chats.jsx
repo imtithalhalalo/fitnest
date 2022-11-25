789fedce71ed9df84714f40d9d9c275b0a709baa
imtithalhalalo <halaloimtithal@gmail.com>
2022-11-25 15:40:07 +0200

    frontend: adding redirect to phone call feature

--- fitnest-frontend/src/screens/user/Chat/Chats.jsx
+++ fitnest-frontend/src/screens/user/Chat/Chats.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, FlatList } from 'react-native'
+import { SafeAreaView, FlatList, Linking } from 'react-native'
 import React, { useContext } from 'react'
 import Header from '../../../components/Header';
 import ChatCard from '../../../components/ChatCard';
@@ -13,20 +13,24 @@ const Chats = ({ navigation }) => {
 
     const { userData } = useContext(UserContext);
     const { response, loading, error } = useGetTrainers();
-
-    const createChat = (id, image, name ) => {
+    let message = `Hello, my name is ${userData.name}, I am new here please guide me more in my personal plan that you added...`
+    const createChat = (id, image, name) => {
         console.log(image, name, id)
-            const database = getFirestore();
-            addDoc(collection(database, "chats"), {
-                id: `${userData.id}${id}`,
-                userName: userData.name,
-                trainerName: name,
-                user: userData.id,
-                trainer: id,
-            })
-                .then(() => navigation.navigate('Chat', {trainer_id: id, name: name, image: image, chatID: `${userData.id}${id}`}))
-                .catch((error) => alert(error.message))
-            
+        const database = getFirestore();
+        addDoc(collection(database, "chats"), {
+            id: `${userData.id}${id}`,
+            userName: userData.name,
+            trainerName: name,
+            user: userData.id,
+            trainer: id,
+        })
+            .then(() => navigation.navigate('Chat', { trainer_id: id, name: name, image: image, chatID: `${userData.id}${id}` }))
+            .catch((error) => alert(error.message))
+
+    }
+    const call = (phone_num) => {
+        const url = `tel://${phone_num}`
+        Linking.openURL(url)
     }
 
     return (
@@ -37,7 +41,7 @@ const Chats = ({ navigation }) => {
                     <Loader />
                     :
                     response.length == 0 ? (
-                        <EmptyState image={require("../../../../assets/images/gym.png")} description={"This section will contain available fitness trainers to chat with "} title={"No Available Trainers"}/>
+                        <EmptyState image={require("../../../../assets/images/gym.png")} description={"This section will contain available fitness trainers to chat with "} title={"No Available Trainers"} />
                     ) :
                         <FlatList
                             data={response}
@@ -45,15 +49,17 @@ const Chats = ({ navigation }) => {
                             contentContainerStyle={{ paddingBottom: 200 }}
                             renderItem={
                                 ({ item }) =>
-                                    
-                                      <ChatCard
+
+                                    <ChatCard
                                         id={item.id}
                                         title={item.name}
-                                        image={item.image} 
-                                        createChat={()=>createChat(item.id, item.image, item.name)}
-                                        />  
-                                    
-                                    }
+                                        image={item.image}
+                                        createChat={() => createChat(item.id, item.image, item.name)}
+                                        call={() => call(item.phone_num)}
+                                        sendWhatsappMessage={() => { sendWhatsappMessage(item.phone_num, message) }}
+                                    />
+
+                            }
                         />
             }
         </SafeAreaView>
